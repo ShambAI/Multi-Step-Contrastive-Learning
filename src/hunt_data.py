e37b20287753f23a9db5fd6a5cd169ed3d118f94
Abdul-Kazeem Shamba <shamba@dhcp-10-24-86-115.wlan.ntnu.no>
2024-06-04 18:28:20 +0200

    New draft of TCL

--- src/hunt_data.py
+++ src/hunt_data.py
@@ -389,9 +389,6 @@ class STFTDataset(HARDataset):
         # Randomly select 100 file numbers
         selected_nums = random.sample(file_numbers, number_subject)
 
-        print("selected_nums: ", selected_nums)
-        
-
         # Loop through the selected file numbers
         for file_number in tqdm(selected_nums):
             # Construct the full file path
@@ -407,8 +404,6 @@ class STFTDataset(HARDataset):
 
                 # Randomly select 5 files (or as many as available)
                 selected_files = random.sample(npz_files, num_files_to_select)
-
-                print("selected_files: ", selected_files)
 
                 # Process the selected files
                 for fn in selected_files:
