1ff2c7690ee976808ade76d48f5b3bc238a59b4b
Abdul-Kazeem Shamba <shamba@Abdul-Kazeems-MacBook-Pro.local>
2024-11-18 16:17:41 +0100

    Added New Baseline

--- baselines/src/losses/contrastive.py
+++ baselines/src/losses/contrastive.py
@@ -644,3 +644,63 @@ def InfoNCE(z1, z2, temperature=1.0):
     return loss
 
 
+class SimMTMLoss(nn.Module):
+
+    def __init__(self, device, temperature):
+        super(SimMTMLoss, self).__init__()
+        self.device = device
+        self.temperature = temperature
+        
+        self.bce = torch.nn.BCELoss()
+        self.softmax = torch.nn.Softmax(dim=-1)
+        self.log_softmax = torch.nn.LogSoftmax(dim=-1)
+        
+        self.kl = torch.nn.KLDivLoss(reduction='batchmean')
+        
+    def get_positive_and_negative_mask(self, similarity_matrix, cur_batch_size, oral_batch_size):
+        
+        diag = np.eye(cur_batch_size)
+        mask = torch.from_numpy(diag)
+        mask = mask.type(torch.bool)
+        
+        positives_mask = np.zeros(similarity_matrix.size())
+        for i in range(cur_batch_size//oral_batch_size):
+            ll = np.eye(cur_batch_size, cur_batch_size, k=oral_batch_size*i)
+            lr = np.eye(cur_batch_size, cur_batch_size, k=-oral_batch_size*i)
+            positives_mask += ll
+            positives_mask += lr
+        
+        positives_mask = torch.from_numpy(positives_mask)
+        positives_mask[mask] = 0
+
+        negatives_mask = 1 - positives_mask
+        negatives_mask[mask] = 0
+        
+        return positives_mask.type(torch.bool), negatives_mask.type(torch.bool)
+
+    def forward(self, batch_emb_om, batch_x):
+        
+        cur_batch_shape = batch_emb_om.shape
+        oral_batch_shape = batch_x.shape
+        
+        # get similarity matrix among mask samples
+        norm_emb = F.normalize(batch_emb_om, dim=1)
+        similarity_matrix = torch.matmul(norm_emb, norm_emb.transpose(0, 1))
+        
+        # get positives and negatives similarity
+        positives_mask, negatives_mask = self.get_positive_and_negative_mask(similarity_matrix, cur_batch_shape[0], oral_batch_shape[0])
+
+        positives = similarity_matrix[positives_mask].view(cur_batch_shape[0], -1)
+        negatives = similarity_matrix[negatives_mask].view(cur_batch_shape[0], -1)
+        
+        # generate predict and target probability distributions matrix
+        logits = torch.cat((positives, negatives), dim=-1) 
+        y_true = torch.cat((torch.ones(cur_batch_shape[0], positives.shape[-1]) / positives.shape[-1],  torch.zeros(cur_batch_shape[0], negatives.shape[-1])), dim=-1).to(self.device).float()
+        
+        # multiple positives - KL divergence
+        predict = self.log_softmax(logits / self.temperature)
+        loss = self.kl(predict, y_true)
+        
+        return loss, similarity_matrix, logits
+
+
